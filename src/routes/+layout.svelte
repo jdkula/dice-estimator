fad1aeab88b258f3aff457036a63476391603295
Jonathan Kula <jona.d.kula@gmail.com>
2023-03-18 04:44:30 -0700

    Add one smaller screen size

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css';
 </script>
 
-<main class="w-full mx-auto xl:max-w-6xl lg:max-w-4xl md:max-w-2xl max-w-md mt-8">
+<main class="w-full mx-auto xl:max-w-6xl lg:max-w-4xl md:max-w-2xl sm:max-w-md max-w-sm mt-8">
   <slot />
 </main>
